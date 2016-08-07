24ebbc6b454cfc5fe0be054076a03d69be077ae9
Leo Shine <shine.leo@gmail.com>
2016-08-07 14:07:23 +0100

    Small change

--- src/Workspaces/Core/Portable/Shared/Extensions/INamedTypeSymbolExtensions.cs
+++ src/Workspaces/Core/Portable/Shared/Extensions/INamedTypeSymbolExtensions.cs
@@ -374,9 +374,8 @@ namespace Microsoft.CodeAnalysis.Shared.Extensions
             ISymbol within)
         {
             var systemAttributeType = compilation.AttributeType();
-            //TODO there is almost certainly a standard place to get the comparer from
+            
             HashSet<string> propertyNames = new HashSet<string>(StringComparer.Ordinal);
-
             foreach (var type in attributeSymbol.GetBaseTypesAndThis())
             {
                 if (type.Equals(systemAttributeType))
